04bdfc19b845bc6b9a9648fd1c2fe233dfa7755d
Aaron Larner <alarner@users.noreply.github.com>
2023-01-25 17:12:11 -0800

    Fix tests

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,14 +1,18 @@
 import path from "path";
 import { server } from "./server";
+import { describe, test, expect } from "vitest";
 
 describe("server", () => {
-	test("it should return a new Koa server", async () => {
-		const s = await server({
-			rootDirectory: path.join(__dirname, "__fixtures__"),
-			routes: { directory: "./routes" },
-		});
-		expect("instance" in s).toEqual(true);
-		expect("app" in s).toEqual(true);
-		s.instance.close();
-	});
+  test("it should return a new Koa server", async () => {
+    const s = await server({
+      rootDirectory: path.join(__dirname, "__fixtures__"),
+      routes: { directory: "./routes" },
+      server: {
+        port: 5544,
+      },
+    });
+    expect("instance" in s).toEqual(true);
+    expect("app" in s).toEqual(true);
+    s.instance.close();
+  });
 });
